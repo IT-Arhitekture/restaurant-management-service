01e4e649b5334be4372131776b01d32dfe2b33ee
FilipLesjak <filip.lesjak@student.um.si>
2024-03-08 09:06:29 +0100

    init commit

---
+++ restaurant-management-service/src/test/java/com/itarhitekture/restaurantmanagementservice/RestaurantManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itarhitekture.restaurantmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
